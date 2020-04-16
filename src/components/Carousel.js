160fd9135b7db7a67727f3092c39f8b20e3086ca
Alex Travick <alextravick18@gmail.com>
2020-04-16 12:52:02 -0400

    start build of pages, components

---
+++ src/components/Carousel.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Card from '../components/Card';
+
+import techbar from '../img/screencapture-tech-bar-netlify-com-1585967985030.png';
+
+class Carousel extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [
+        {
+          id: 0,
+          title: 'Tech Bar',
+          subTitle: 'Your favorite drink finder!',
+          imgSrc: techbar,
+          link: 'https://tech-bar.netlify.com',
+          selected: false
+        },
+        {
+          id: 1,
+          title: '',
+          subTitle: '',
+          imgSrc: '',
+          link: '',
+          selected: false
+        },
+        {
+          id: 2,
+          title: '',
+          subTitle: '',
+          imgSrc: '',
+          link: '',
+          selected: false
+        }
+      ]
+    }
+  }
+
+  handleCardClick= (id, card) => {
+    let items = [...this.state.items];
+
+    // Grabs items, goes to item with same ID as card and toggles Selected prop
+    items[id].selected = items[id].selected ? false : true;
+
+    items.forEach(item => {
+      if (item.id !== id) {
+        item.selected = false;
+      }
+    });
+
+    this.setState(
+      items
+    );
+  }
+
+
+  makeItems = (items) => {
+    return items.map(item => {
+      return <Card item={item} onClick={(e => this.handleCardClick(item.id, e))} key={item.id} />
+    })
+  }
+
+  render() {
+    return (
+      <Container fluid={true}>
+        <Row className="justify-content-around">
+          {this.makeItems(this.state.items)}
+        </Row>
+      </Container>
+    )
+  }
+
+}
+
+export default Carousel;
